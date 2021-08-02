4b367018b5bdcd14bb39d8a09fe569dfbbca91b6
euncharm1ng <21700287@handong.edu>
2021-08-02 16:48:56 +0900

    starting SNS

---
+++ gui.py
@@ -0,0 +1,20 @@
+'''
+part where connects the board to the mctsEva.py where calculations and decisions are made.
+receives user input and displays the calculated moves.
+using the board developed by eagleflo/goban, https://github.com/eagleflo/goban
+'''
+
+
+def usrInput():
+    #todo
+
+
+def aiOutput():
+    #todo
+
+
+def selectStone():
+    #todo
+
+def exportBoard():
+    #todo
---
+++ mctsEva.py
@@ -0,0 +1,45 @@
+'''
+where monte carlo tree search is taking place. 
+selection
+expansion
+sumulation (rollout)
+backpropagation 
+refereced: 
+    https://www.analyticsvidhya.com/blog/2019/01/monte-carlo-tree-search-introduction-algorithm-deepmind-alphago/
+    https://towardsdatascience.com/monte-carlo-tree-search-an-introduction-503d8c04e168
+'''
+
+board[19][19] #int
+log[]
+
+
+def calcUCB():
+    #todo
+
+
+def expansion():
+    #todo
+
+
+def backprop():
+    #todo
+
+
+def rollout():
+    #todo
+
+
+def select():
+    #todo
+
+
+def chkTime():
+    #todo
+
+
+def chkVic():
+    #todo
+
+
+def undo():
+    #todo
