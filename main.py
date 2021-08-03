ea0324aa070358c383d06acbce9aae34543d251e
euncharm1ng <21700287@handong.edu>
2021-08-03 21:21:08 +0900

    before pull

--- main.py
+++ main.py
@@ -3,18 +3,130 @@ hello world from me!
 """
 import gui, go, goban, mctsEva
 
-a = mctsEva.node("root")
-b = mctsEva.node(a)
-c = mctsEva.node(a)
-
 print("start")
 
-a.addC(b)
-a.addC(c)
-b.setName("im bc")
-c.setName("im c")
-a.printC()
+a = mctsEva.node("root")
+b = mctsEva.node(a)
+# a.board[1][1] = 1
+# a.board[1][2] = 1
+# a.board[1][3] = 2
+# a.board[1][4] = 1
+# a.board[1][5] = 1
+# a.board[1][6] = 1
+# a.board[0][15] = 2
+# a.board[1][7] = 2
+# a.board[17][13] = 1
+# a.board[17][14] = 2
+# a.board[17][15] = 2
+# a.board[17][16] = 2
+# a.board[17][17] = 2
+# a.board[17][18] = 2
+
+# a.board[0][1] = 4
+# a.board[2][1] = 1
+# a.board[3][1] = 1
+# a.board[4][1] = 2
+# a.board[5][1] = 1
+# a.board[6][1] = 1
+# a.board[13][18] = 1
+# a.board[14][18] = 1
+# a.board[15][18] = 1
+# a.board[16][18] = 1
+# a.board[17][18] = 1
+# a.board[18][18] = 1
+
+# a.board[1][1] = 1
+# a.board[2][2] = 1
+# a.board[3][3] = 1
+# a.board[4][4] = 1
+# a.board[5][5] = 1
+# a.board[6][6] = 2
+# a.board[18][18] = 2
+
+# a.board[6][10] = 2
+# a.board[7][11] = 2
+# a.board[8][12] = 2
+# a.board[9][13] = 1
+# a.board[10][14] = 2
+# a.board[11][15] = 2
+
+# a.board[12][5] = 2
+# a.board[13][6] = 2
+# a.board[14][7] = 2
+# a.board[15][8] = 1
+# a.board[16][9] = 2
+# a.board[17][10] = 2
+
+a.board[0][13] = 2
+a.board[1][14] = 2
+a.board[2][15] = 2
+a.board[3][16] = 2
+a.board[4][17] = 2
+a.board[5][18] = 2
+
+a.board[13][0] = 1
+a.board[14][1] = 1
+a.board[15][2] = 1
+a.board[16][3] = 2
+a.board[17][4] = 1
+a.board[18][5] = 1
+
+a.board[0][5] = 1
+a.board[1][4] = 1
+a.board[2][3] = 1
+a.board[3][2] = 2
+a.board[4][1] = 1
+a.board[5][0] = 1
+
+a.board[9][9] = 1
+a.board[10][8] = 1
+a.board[11][7] = 2
+a.board[12][6] = 1
+a.board[13][5] = 1
+a.board[14][4] = 1
+
+a.board[0][18] = 1
+a.board[1][17] = 1
+a.board[2][16] = 1
+a.board[3][15] = 1
+a.board[4][14] = 1
+a.board[5][13] = 1
+
+# a.board[10][18] = 1
+# a.board[11][17] = 1
+# a.board[12][16] = 2
+# a.board[13][15] = 2
+# a.board[14][14] = 2
+# a.board[15][13] = 1
+
+# a.board[10][10] = 1
+# a.board[11][9] = 1
+# a.board[12][8] = 1
+# a.board[13][7] = 1
+# a.board[14][6] = 2
+# a.board[15][5] = 1
+
+# a.board[18][10] = 1
+# a.board[17][11] = 1
+# a.board[16][12] = 2
+# a.board[15][13] = 1
+# a.board[14][14] = 1
+# a.board[13][15] = 1
+
+# a.board[18][17] = 1
+
+mcts = mctsEva.mcts()
+
+mcts.printB(a)
+print("middle")
+# mcts.findMoves(a)
+# mcts.printAvailM(a)
+print(mcts.chkVic(a))
+
+
 
 print("end")
+
+
 
 
--- mctsEva.py
+++ mctsEva.py
@@ -11,17 +11,38 @@ refereced:
 '''
 todo: 
     URGENT: ds
+    decide t according to consequences
 
 '''
+from numpy import log as ln
+import numpy
+import math, copy
+
+movRange = 2
+
+class move:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "x: " + str(self.x) + " y: " + str(self.y)
 
 class node:
-    def __init__(self, parent):
-        self.t = 0
-        self.n = 0
-        self.ucb = 0
+    def __init__(self, parent):#, mov1, mov2):
+        self.t = 0 # value 
+        self.n = 0 # number of times visited
+        self.ucb = float('inf')
         self.name = ""
         self.children = []
         self.parent = parent
+        #self.mov1 = mov1
+        #self.mov2 = mov2
+        if(parent == "root"):
+            self.board = numpy.zeros((19,19), dtype = int)
+        else: 
+            self.board = copy.deepcopy(parent.board)
+        self.availMov = []
 
     def addC(self, child):
         self.children.append(child)
@@ -34,19 +55,19 @@ class node:
         self.name = name
 
     def __str__(self):
-        return self.name #"ucb value is : " + str(self.ucb)
+        return "ucb value is : " + str(self.ucb) #self.name #
 
 
 
 class mcts: 
     def __init__(self):
-        board = [19][19] #int
+        #self.board = numpy.zeros((19,19)) #int assuming 0 is empty and 1,2,3 for b, w, r
         pass
         #todo
 
-    #1 
-    def calcUCB(self, node, userinput):
-        pass
+    #1 no need return, update node.ucb
+    def calcUCB(self, node):
+        node.ucb = node.t + 2*(math.sqrt(ln(node.parent.n)/node.n))
         #todo
 
     #1
@@ -59,10 +80,29 @@ class mcts:
         pass
         #todo
 
-    #2
-    def rollout(self):
-        pass
-        #todo
+    #2 
+    def rollout(self, currNode):
+
+       ''' tempnode = copy.deepcopy(self)
+
+		while True :
+			self.findMoves(tempnode)
+			#choose randome next move and move 2 stones
+			self.availMov
+			if self.chkVic(tempnode):
+				break
+
+		return self.t = 20
+
+        pass'''
+        #todo find available moves -> make random move for ai -> check victory -> findMoves -> make random move for user -> check victory -> find -> .... -> somebody won -> update current node's t value 
+
+        #findMoves(tempnode)
+        #random 2 moves = (tempnode.availMov)
+        #tempnode.board[random 2 moves] = 1 if it's ai's turn, 2 if it's user's turn
+        #check victory
+        #findMoves
+
 
     #5
     def select(self):
@@ -70,25 +110,176 @@ class mcts:
         #todo
 
     #2 input: board, output: available moves
-    def findMoves(self):
-        pass
-        #todo
+    def findMoves(self, node):
+        moveBoard = numpy.ones((19,19), dtype = int)
+        for i in range(19):
+            for j in range(19):
+                if node.board[i][j] :
+                    moveBoard[i][j] = 0
+                    for k in range(self.ifSmaller(i-movRange), self.ifBigger(i+movRange+1)):
+                        for l in range(self.ifSmaller(j-movRange), self.ifBigger(j+movRange+1)):
+                            moveBoard[k][l] *= 2
+
+        moveBoard[moveBoard < 2] = 0
+
+        for i in range(19):
+            for j in range(19):
+                print(str(moveBoard[i][j]) + " ", end = "")
+                if moveBoard[i][j]:
+                    node.availMov.append(move(i,j))
+            print("")
+        
+    def printAvailM(self, node):
+        for mov in node.availMov:
+            print(mov)
 
 
+    def ifBigger(self, num):
+        if num > 19 :
+            return 19
+        else :
+            return num
 
-class checker:
-    log=[] #for undo 
+
+    def ifSmaller(self, num):
+        if num < 0 :
+            return 0
+        else :
+            return num
+        
+
+    def printB(self, node):
+        for i in range(19):
+            for j in range(19):
+                print(str(node.board[i][j]) + " ", end = "")
+            print("")
+
+
     def chkTime(self):
         pass
         #todo
 
 
-    #input: board, output: bool, true for win
-    def chkVic(self):
-        pass
-        #todo
+    #input: node, output: bool, true for a winner
+    def chkVic(self, node):
+        tag = 0
+        cnt = 0
+        #dir1 horizontal
+        i = 0
+        while i < 19:
+            j = 0
+            while j < 19:
+                if node.board[i][j]:
+                    tag = node.board[i][j]
+                    while j < 19 and tag == node.board[i][j]:
+                        cnt += 1
+                        j = j + 1
+                    #print("outside while, cnt = " + str(cnt) + " " + str(j))
+                    if cnt > 5:
+                        return True, tag
+                    else:
+                        cnt = 0
+                        j -= 1
+                j+=1
+            i+=1
+        #dir2 vertical
+        j = 0
+        while j < 19:
+            i = 0
+            while i < 19:
+                if node.board[i][j]:
+                    tag = node.board[i][j]
+                    while i < 19 and tag == node.board[i][j]:
+                        cnt += 1
+                        i = i + 1
+                    #print("outside while, cnt = " + str(cnt) + " " + str(i))
+                    if cnt > 5:
+                        return True, tag
+                    else:
+                        cnt = 0
+                        i -= 1
+                i+=1
+            j+=1
+        #return False, 0
+        #dir3 diag top left to bot right
+        for i in range(14):
+            j = 0 
+            while i < 19:
+                #print("loop var i : "+ str(i))
+                if node.board[i][j]:
+                    tag = node.board[i][j]
+                    while i < 19 and tag == node.board[i][j]:
+                        cnt += 1
+                        i += 1
+                        j += 1
+                    print("outside while, cnt = " + str(cnt) + " " + str(i) + " " + str(j))
+                    if cnt > 5:
+                        return True, tag
+                    else:
+                        cnt = 0
+                        continue
+                j += 1
+                i += 1
+
+        for j in range (1, 14):
+            i = 0
+            while j < 19:
+                if node.board[i][j]:
+                    tag = node.board[i][j]
+                    while j < 19 and tag == node.board[i][j]:
+                        cnt += 1
+                        i += 1
+                        j += 1
+                    print("outside while, cnt = " + str(cnt) + " " + str(i) + " " + str(j))
+                    if cnt > 5:
+                        return True, tag
+                    else: 
+                        cnt = 0
+                        continue
+                j += 1
+                i += 1
+
+        #dir4 diag top right to bot left
+        for j in range (5, 19):
+            i = 0 
+            while j > -1:
+                if node.board[i][j]:
+                    tag = node.board[i][j]
+                    while j > -1 and tag == node.board[i][j]: 
+                        cnt += 1
+                        i += 1
+                        j -= 1
+                    print("outside while, cnt = " + str(cnt) + " " + str(i) + " " + str(j))
+                    if cnt > 5:
+                        return True, tag
+                    else:
+                        cnt = 0
+                        continue
+                i += 1
+                j -= 1
+
+        for i in range (1, 14):
+            j = 0
+            while i < 19:
+                if node.board[i][j]:
+                    tag = node.board[i][j]
+                    while i < 19 and tag == node.board[i][j]:
+                        cnt += 1
+                        i += 1
+                        j -= 1
+                    print("outside while, cnt = " + str(cnt) + " " + str(i) + " " + str(j))
+                    if cnt > 5:
+                        return True, tag
+                    else: 
+                        cnt = 0
+                        continue
+                i += 1
+                j -= 1
+
+        return False, 0
 
 
     def undo(self):
         pass
         #todo later`
+
